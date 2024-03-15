b2465a66fd07da7ce0fb15eebda466aa86325001
Or Eliyahu <or.alef@gmail.com>
2024-03-15 10:18:43 +0200

    improved nvim config

---
+++ config/nvim/lua/custom/plugins/filetree.lua
@@ -0,0 +1,24 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {
+      close_if_last_window = true,
+      window = {
+        mappings = {
+          ['l'] = 'open',
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    }
+  end,
+}
--- config/nvim/lua/user/comment.lua
+++
@@ -1,9 +0,0 @@
--- Enable Comment.nvim
-require('Comment').setup()
-
--- Enable `lukas-reineke/indent-blankline.nvim`
--- See `:help indent_blankline.txt`
-require('indent_blankline').setup {
-  char = '┊',
-  show_trailing_blankline_indent = false,
-}
