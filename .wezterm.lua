7a4cd7cf27d39d42b5a214b0bfd95c1be03528e0
Or Eliyahu <or.alef@gmailc.om>
2024-06-03 11:54:07 +0300

    support for sigint

--- .wezterm.lua
+++ .wezterm.lua
@@ -16,8 +16,35 @@ config.hide_tab_bar_if_only_one_tab = true
 config.initial_rows = 50
 config.initial_cols = 232
 config.keys = {
-	{ key = "v", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) },
-	{ key = "c", mods = "CTRL", action = wezterm.action({ CopyTo = "Clipboard" }) },
+	{
+		key = "c",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(wezterm.action({ CopyTo = "ClipboardAndPrimarySelection" }), pane)
+				window:perform_action("ClearSelection", pane)
+			else
+				window:perform_action(wezterm.action({ SendKey = { key = "c", mods = "CTRL" } }), pane)
+			end
+		end),
+	},
+	{
+		key = "c",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action({ SendKey = { key = "c", mods = "CTRL" } }),
+	},
+	{
+		key = "v",
+		mods = "CTRL",
+		action = wezterm.action({ PasteFrom = "Clipboard" }),
+	},
+	{
+		key = "v",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action({ SendKey = { key = "v", mods = "CTRL" } }),
+	},
 }
+config.scrollback_lines = 3500
 
 return config
